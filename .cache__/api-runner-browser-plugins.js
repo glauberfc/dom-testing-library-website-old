1e9e8c17988c46f86cfc5ec24cc6bcbd1742d096
Glauber Castro <gfc714@gmail.com>
2018-09-26 15:02:36 -0300

    Remove unused folders

--- .cache__/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('D:/Dev/Dom-testing-library/dom-testing-library-website/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    }]
